fdd3302536a100f916d5027317ffd0a9a5e1decc
Alexander Almeida <alexanderalmeida3040@gmail.com>
2022-08-02 15:25:32 -0300

    Sections Added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import AboutSection from '../components/AboutSection/AboutSection'
 import ServicesSection from '../components/ServicesSection/ServicesSection'
+import FaqSection from '../components/FaqSection'
 
 const Home: NextPage = () => {
   return (
     <>
       <AboutSection/>
       <ServicesSection/>
+      <FaqSection/>
     </>
   )
 }
