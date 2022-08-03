1f60d2c274b02427f4a667d44d5eefe5e8f958e8
Alexander Almeida <alexanderalmeida3040@gmail.com>
2022-08-03 15:22:11 -0300

    styled components added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,11 @@ import type { NextPage } from 'next'
 import AboutSection from '../components/AboutSection/AboutSection'
 import ServicesSection from '../components/ServicesSection/ServicesSection'
 import FaqSection from '../components/FaqSection'
-
+import GlobalStyle from '../styles/GlobalStyle'
 const Home: NextPage = () => {
   return (
     <>
+      <GlobalStyle/>
       <AboutSection/>
       <ServicesSection/>
       <FaqSection/>
